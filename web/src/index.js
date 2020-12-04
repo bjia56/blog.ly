7006111d0238ec7e442363e4bcb649fab21e2f6f
Brett Jia <dev.bjia56@gmail.com>
2020-12-03 23:27:34 -0500

    remove unused import

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import axios from 'axios'
+//import axios from 'axios'
 
 //axios.defaults.baseURL = 'http://localhost:3000/'
 
