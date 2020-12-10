1c69e2c9e0d7f86029a79ba05f8454e4523097d8
Zixiong Liu <zl2683@columbia.edu>
2020-12-11 02:30:04 +0800

    implement search

--- services/SearchService.js
+++ services/SearchService.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service')
 
+const { Client } = require('../elastic')
+
 /**
  * Search for blogs
  *
@@ -11,11 +13,53 @@ const Service = require('./Service')
  * */
 const apiSearchGET = ({ keyword, cursor, limit }) =>
     new Promise(async (resolve, reject) => {
-        // try {
-        resolve(Service.successResponse())
-        // } catch (e) {
-        //    reject(Service.rejectResponse(e))
-        //}
+        try {
+            Client.search({
+                index: 'blogs',
+                body: {
+                    query: {
+                        bool: {
+                            should: {
+                                match_phrase: {
+                                    title: keyword,
+                                },
+                            },
+                            should: {
+                                match_phrase: {
+                                    contents: keyword,
+                                },
+                            },
+                        },
+                    },
+                    highlight: {
+                        fields: {
+                            contents: {},
+                        },
+                        encoder: 'html',
+                        pre_tags: ['<mark>'],
+                        post_tags: ['</mark>'],
+                        number_of_fragments: 0,
+                    },
+                },
+            }).then((resp) => {
+                if (resp.body.hits) {
+                    const ret = resp.body.hits.hits.map((hit) => {
+                        return {
+                            blog: hit._source.uuid,
+                            title: hit._source.title,
+                            contents: hit.highlight.contents[0],
+                            author: hit._source.author,
+                            authorName: hit._source.authorName,
+                        }
+                    })
+                    resolve(Service.successResponse(ret))
+                }
+                resolve(Service.successResponse([]))
+            })
+        } catch (e) {
+            console.log(e)
+            reject(Service.rejectResponse(e))
+        }
     })
 
 module.exports = {
